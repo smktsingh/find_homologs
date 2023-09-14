2f4b33dc4227575bb7c7c57009887624d0566706
Samyukta <smktsingh@gmail.com>
2023-09-13 22:33:44 -0400

    Question 10

--- find_homologs.sh
+++ find_homologs.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-makeblastdb -in "$2" -dbtype nucl -out database
-blastn -query "$1" -db database -out "$3"
-grep -c 'Sbjct' results.txt
+tblastn -query "$1" -subject "$2" -outfmt "6 qseqid sseqid pident length mismatch gapopen qstart qend sstart send evalue bitscore qlen" | awk '$3 > 30 && $4/$13 * 100 > 90 {print $0} ' > "$3"
+wc=$(wc -l < "$3")
+echo $wc
+
